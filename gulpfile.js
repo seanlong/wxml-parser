9c7ec45cca86e668cf233d9cc4d2abf7dbce1ac7
Long Xiang <seanlong@outlook.com>
2016-10-27 16:28:16 +0800

    Build for ES5 support and change watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,52 @@
+var babelify = require('babelify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var watchify = require('watchify');
+
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var babel = require('babelify');
-var path = require('path');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+var plugins = require('gulp-load-plugins')();
 
-function compile(entry, output, watch) {
-  var bundler = watchify(browserify(entry, { debug: true }).transform(babel));
+var options = {
+  debug: false,
+  fullPaths: false
+};
 
-  function rebundle() {
-    bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source(output))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build'));
-  }
-
-  if (watch) {
-    bundler.on('update', function() {
-      console.log('-> bundling...');
-      rebundle();
-    });
-  }
-
-  rebundle();
+function createBundler(entry) {
+  options = plugins.util.env.production ? options :
+    assign(options, { debug: true, fullPaths: true, cache: {}, packageCache: {} });
+  options.entries = [entry];
+  var b = browserify(options);
+  b.transform('babelify', { presets: ['es2015'] });
+  return b;
 }
 
-gulp.task('build', function() { return compile('./index.js', 'wxml-parser.js'); });
-gulp.task('watch', function() { return compile('./index.js', 'wxml-parser.js', true); });
-gulp.task('test', function() { return compile('./test/test.js', 'test.js', true); })
+function bundle(b, out) {
+  return b.bundle()
+    .on('error', plugins.util.log.bind(plugins.util, 'Browserify Error'))
+    .pipe(source(out))
+    .pipe(buffer())
+    .pipe(plugins.util.env.production ? uglify() : gutil.noop())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist'));
+}
 
-gulp.task('default', ['watch']);
+gulp.task('build', function() {
+  var bundler = createBundler('index.js');
+  return bundle(bundler, 'wxml-parser.js')
+});
+
+gulp.task('test', function() {
+  var bundler = createBundler('test/test.js');
+  bundler.plugin(watchify);
+  bundler.on('update', function() {
+    bundle(bundler, 'test.js');
+    gutil.log('Rebundle...');
+  });
+  bundle(bundler, 'test.js');
+});
